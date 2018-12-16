c88c836c0e0214fc052385071eb5a8bbf2298aae
SH <sh@example.com>
2018-12-16 21:53:46 +0000

    Extract file path from input arguments

--- src/main/java/Arguments.java
+++ src/main/java/Arguments.java
@@ -1,6 +1,23 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Arguments {
+    private final Path filePath;
+
+    public Arguments(Path filePath) {
+        this.filePath = filePath;
+    }
+
+    public Path getFilePath() {
+        return filePath;
+    }
+
     public static Arguments parse(String[] args) {
-        throw new InvalidArgumentException();
+        if (args.length == 0) {
+            throw new InvalidArgumentException();
+        }
+
+        return new Arguments(Paths.get(args[0]));
     }
 
     public static class InvalidArgumentException extends RuntimeException {
--- src/test/java/ArgumentsTest.java
+++ src/test/java/ArgumentsTest.java
@@ -1,6 +1,17 @@
 import org.junit.Test;
 
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
 public class ArgumentsTest {
+    @Test
+    public void parsesValidFilePath() {
+        Arguments arguments = Arguments.parse(new String[] {"someFilePath", "origin", "7", "someNewValue"});
+
+        assertEquals(Paths.get("someFilePath"), arguments.getFilePath());
+    }
+
     @Test(expected = Arguments.InvalidArgumentException.class)
     public void rejectsEmptyArguments() {
         Arguments.parse(new String[] {});
